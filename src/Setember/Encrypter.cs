c07d20d30a505fed4e40549fec45c7d61782a177
Daniel Aspect <daniel_aspect@proton.me>
2023-09-02 20:37:07 -0300

    Added license and legal warning to source code

---
+++ src/Setember/Encrypter.cs
@@ -0,0 +1,20 @@
+// 
+//   ____ __________ _____ __  __ ____ _____ ____  
+//  / ___|___ /_   _|___ /|  \/  | __ )___ /|  _ \ 
+//  \___ \ |_ \ | |   |_ \| |\/| |  _ \ |_ \| |_) |
+//   ___) |__) || |  ___) | |  | | |_) |__) |  _ < 
+//  |____/____/ |_| |____/|_|  |_|____/____/|_| \_\
+//                                                 
+//
+// Copyright (c) 2023 Daniel Aspect
+// 
+// This file is part of the "Setember" project, licensed under The MIT License.
+// 
+// THIS SOFTWARE MUST ONLY BE USED FOR EDUCATIONAL PURPOSES!
+
+namespace Setember;
+
+public class Encrypter
+{
+
+}
