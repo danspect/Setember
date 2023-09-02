595617a51763980b713cd934dc7d0ea69d3bf351
Daniel Aspect <daniel_aspect@proton.me>
2023-09-02 20:37:31 -0300

    Added license and legal warning to source code

--- src/Setember/FileManager.cs
+++ src/Setember/FileManager.cs
@@ -1,3 +1,17 @@
+// 
+//   ____ __________ _____ __  __ ____ _____ ____  
+//  / ___|___ /_   _|___ /|  \/  | __ )___ /|  _ \ 
+//  \___ \ |_ \ | |   |_ \| |\/| |  _ \ |_ \| |_) |
+//   ___) |__) || |  ___) | |  | | |_) |__) |  _ < 
+//  |____/____/ |_| |____/|_|  |_|____/____/|_| \_\
+//                                                 
+//
+// Copyright (c) 2023 Daniel Aspect
+// 
+// This file is part of the "Setember" project, licensed under The MIT License.
+// 
+// THIS SOFTWARE MUST ONLY BE USED FOR EDUCATIONAL PURPOSES!
+
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
